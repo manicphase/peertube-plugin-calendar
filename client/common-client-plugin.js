d6cce9969adbcd314666b090658e34eaa0402445
manicphase <thegr8dave@gmail.com>
2022-07-15 22:48:57 +0100

    client route test

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,4 +1,32 @@
 function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
+
+  registerHook({
+    target: 'filter:left-menu.links.create.result',
+    handler: (result) => {
+      return [
+        {
+          key: 'in-my-stuff',
+          title: 'In my stuff',
+          links: [
+            {
+              path: '/about',
+              icon: 'alert',
+              shortLabel: 'About',
+              label: 'About'
+            },
+
+            {
+              path: peertubeHelpers.getBasePluginClientPath() + '/my-super/route',
+              icon: '',
+              shortLabel: 'super route',
+              label: 'Super route'
+            }
+          ]
+        }
+      ].concat(result)
+    }
+  })
+
   registerClientRoute({
     route: 'calendar',
     onMount: ({ rootEl }) => {
