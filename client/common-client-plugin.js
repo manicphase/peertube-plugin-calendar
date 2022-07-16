170a8374c5788f1f352895c0d20a68c82113c606
manicphase <thegr8dave@gmail.com>
2022-07-16 10:13:54 +0100

    playing with embed api

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,5 +1,3 @@
-import { PeerTubePlayer } from '@peertube/embed-api'
-
 async function getLatestVideos () {
   let path = '/api/v1/videos?start=0&count=25&sort=-publishedAt&skipCount=true&isLocal=true&nsfw=false'; 
   let request = await fetch(path);
@@ -45,7 +43,7 @@ function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
           response.data[i].startTime = getStartTime(response.data[0]);
           response.data[i].endTime = response.data[i].startTime + (response.data[i].duration * 1000); 
         }
-        rootEl.innerHTML = '<iframe title="Plansformer coding session - 7/13/2022, 10:26:20 PM" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
+        rootEl.innerHTML = '<script src="https://unpkg.com/@peertube/embed-api/build/player.min.js"></script><iframe title="Plansformer coding session - 7/13/2022, 10:26:20 PM" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
         window.response = response;
         //rootEl.innerHTML = getStartTime(response.data[0]) + " " + getStartTime(response.data[1]);
       })
