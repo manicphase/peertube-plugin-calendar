154fea42ddbd1610b5ba4aadb7de0503a6bbafe2
manicphase <thegr8dave@gmail.com>
2022-07-16 19:03:49 +0100

    webpack?

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -48,7 +48,13 @@ function updateMiniVideos() {
       console.log(i);
       if (!document.getElementById(window.currentVideos[i].uuid)) {
         if (window.currentVideos[i].uuid !== window.mainVideoStats.uuid) {
-          minividdiv.appendChild(`<div style="width:200px;" onclick='setAsMainVideo("${window.currentVideos[i].uuid}")'id="${window.currentVideos[i].uuid}_div">${makeEmbedCode(window.currentVideos[i].uuid)}</div>`)
+          let el = document.createElement("div")
+          el.setAttribute("style", "width:200px");
+          el.setAttribute("onclick", `setAsMainVideo("${window.currentVideos[i].uuid}")`)
+          el.setAttribute("id", `"${window.currentVideos[i].uuid}_div"`)
+          el.innerHTML = makeEmbedCode(window.currentVideos[i].uuid)
+          minividdiv.appendChild(el)
+          //minividdiv.appendChild(`<div style="width:200px;" onclick='setAsMainVideo("${window.currentVideos[i].uuid}")'id="${window.currentVideos[i].uuid}_div">${makeEmbedCode(window.currentVideos[i].uuid)}</div>`)
         }
       }
   }
