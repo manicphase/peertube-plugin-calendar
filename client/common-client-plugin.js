fecd91cbbdd2381c6fd705bd7a71e493ab7b01b7
manicphase <thegr8dave@gmail.com>
2022-07-16 14:10:03 +0100

    webpack?

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -57,7 +57,7 @@ function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
         for (let i=0; i<response.data.length; i++) {
           response.data[i].startTime = getStartTime(response.data[0]);
           response.data[i].endTime = response.data[i].startTime + (response.data[i].duration * 1000); 
-          vidlist = vidlist + `<div onclick="changeMainVideo("${response.data[i].uuid.trim()}")>${response.data[i].name}</div>`;
+          vidlist = vidlist + `<div onclick="changeMainVideo("${response.data[i].uuid.trim()}") id="${response.data[i].shortUUID}">${response.data[i].name}</div>`;
           console.log(response.data[i].uuid.trim());
         }
         let vidlistdiv = document.getElementById("vidlist");
