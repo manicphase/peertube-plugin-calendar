c5e3b77e16d0127742a569b4aaf48c8b95a856d0
manicphase <thegr8dave@gmail.com>
2022-07-16 13:28:36 +0100

    webpack?

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -38,13 +38,20 @@ function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
   registerClientRoute({
     route: '/calendar',
     onMount: ({ rootEl }) => {
+      rootEl.innerHTML = '<div id="mainpanel"><div>Blah</div><div id="mainvideo"></div></div>'
+      window.PeerTubePlayer = PeerTubePlayer;
+
       getLatestVideos().then( function (response) {
         for (let i=0; i<response.data.length; i++) {
           response.data[i].startTime = getStartTime(response.data[0]);
           response.data[i].endTime = response.data[i].startTime + (response.data[i].duration * 1000); 
         }
-        rootEl.innerHTML = '<script src="https://unpkg.com/@peertube/embed-api/build/player.min.js"></script><iframe title="Plansformer coding session - 7/13/2022, 10:26:20 PM" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
-        window.PeerTubePlayer = PeerTubePlayer;
+        let mainvideodiv = document.getElementById("mainvideo")
+        mainvideodiv.innerHTML = '<iframe id="mainplayer" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
+        //rootEl.innerHTML = '<div id="mainpanel"><div id="mainvideo"></div></div>'
+        //rootEl.innerHTML = '<input type="text" id="timestamp"/>'
+        //rootEl.innerHTML = '<iframe id="mainplayer" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
+        //window.PeerTubePlayer = PeerTubePlayer;
         //rootEl.innerHTML = getStartTime(response.data[0]) + " " + getStartTime(response.data[1]);
       })
     }
