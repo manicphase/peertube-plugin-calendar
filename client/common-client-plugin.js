dd3de29646570b87c96f0296788aeb775f9cf8a1
manicphase <thegr8dave@gmail.com>
2022-07-16 10:36:23 +0100

    require not import

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,3 +1,5 @@
+import { PeerTubePlayer } from '@peertube/embed-api';
+
 PeerTubePlayer = require('@peertube/embed-api');
 
 async function getLatestVideos () {
@@ -46,7 +48,7 @@ function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
           response.data[i].endTime = response.data[i].startTime + (response.data[i].duration * 1000); 
         }
         rootEl.innerHTML = '<script src="https://unpkg.com/@peertube/embed-api/build/player.min.js"></script><iframe title="Plansformer coding session - 7/13/2022, 10:26:20 PM" src="https://video.manicphase.me/videos/embed/5dc2bbc0-7eda-4bda-8659-d361795e8fb2?autoplay=1&api=1" allowfullscreen="" sandbox="allow-same-origin allow-scripts allow-popups" width="560" height="315" frameborder="0"></iframe>'
-        window.response = response;
+        window.PeerTubePlayer = PeerTubePlayer;
         //rootEl.innerHTML = getStartTime(response.data[0]) + " " + getStartTime(response.data[1]);
       })
     }
