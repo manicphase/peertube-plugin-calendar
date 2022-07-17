e55f74f6a2f6488130ec51478ba58557d9c575f4
manicphase <thegr8dave@gmail.com>
2022-07-17 17:25:55 +0100

    webpack?

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -157,8 +157,8 @@ function register ({ registerClientRoute, registerHook, peertubeHelpers }) {
         let vidlistdiv = document.getElementById("vidlist");
         vidlistdiv.innerHTML = vidlist;
         const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.timestamp) window.globalTime = urlParams.timestamp;
-        if (urlParams.videoID) setAsMainVideo(urlParams.videoID);
+        if (urlParams.get(timestamp)) window.globalTime = urlParams.get(timestamp);
+        if (urlParams.get(videoID)) setAsMainVideo(urlParams.get(videoID));
         updateMiniVideos();
       })
     }
